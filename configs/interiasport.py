87f34d310a13e2b2f6c68fc83e6a29dcb2a7b8f6
Tomasz Garbus <tomasz.garbus1@gmail.com>
2019-12-01 15:42:30 +0100

    Cleanup articles from javascript

--- configs/interiasport.py
+++ configs/interiasport.py
@@ -1,9 +1,28 @@
+import re
 from typing import Optional
 
 from bs4 import BeautifulSoup
 
 from article import Article
 from configs.scraper_config import ScraperConfig
+
+# Substrings to remove from the text.
+BLACKLIST = [
+    'Odtwarzacz wideo wymaga uruchomienia obsługi JavaScript w przeglądarce.',
+    'Wideo',
+    'Zdjęcie',
+    '//]]',
+]
+JS_PREF = [
+    'var', '$', 'window.', 'if', 'else', '}', 'return'
+]
+JS_SUB = [
+    '.style.', ' = ', '@media', 'Criteo', 'INTERIA.TV', 'player',
+    'function', 'zoneid', 'containerid', 'false', 'true'
+]
+JS_SUF = [
+    ';', '{'
+]
 
 
 class InteriaSportScraperConfig(ScraperConfig):
@@ -22,6 +41,31 @@ class InteriaSportScraperConfig(ScraperConfig):
     def should_follow_link(self, link: str) -> bool:
         return link.startswith(self.home())
 
+    def _is_line_js(self, line: str) -> bool:
+        """
+        A very simple heuristic determining whether the given line of text is
+        JavaScript code.
+        """
+        for pref in JS_PREF:
+            if line.strip().startswith(pref):
+                return True
+        for sub in JS_SUB:
+            if sub in line:
+                return True
+        for suf in JS_SUF:
+            if line.strip().endswith(suf):
+                return True
+        return False
+
+    def _cleanup(self, text: str) -> str:
+        for elem in BLACKLIST:
+            text = re.sub(elem, '', text)
+        lines = text.split('\n')
+        lines = list(filter(lambda l: l.strip() != '', lines))
+        lines = list(filter(lambda l: not self._is_line_js(l), lines))
+        text = '\n'.join(lines)
+        return text
+
     def extract_article(self, soup: BeautifulSoup, source_url: str) -> \
             Optional[Article]:
         title = soup.find('title').text
@@ -39,6 +83,7 @@ class InteriaSportScraperConfig(ScraperConfig):
         if art_body is None:
             return None
         text = art_lead.text + art_body.text
+        text = self._cleanup(text)
         raw_html = str(soup)
         return Article(title, datetime, text, source_url, raw_html)
 
